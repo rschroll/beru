0c3b8040154307aba7f89330f2524e5178eb610f
Robert Schroll <rschroll@gmail.com>
2013-11-24 21:05:03 -0300

    Update click packaging to set architecture

--- makeclick.sh
+++ makeclick.sh
@@ -6,9 +6,19 @@ for dir in Epub File FontList HttpServer html ui apparmor
 do
     cp -r $dir click/
 done
-for file in beru beru.desktop beru.svg manifest.json COPYING README.md
+for file in beru beru.desktop beru.svg COPYING README.md
 do
     cp $file click/
 done
+
+ARCH=`uname -m`
+if [ "$ARCH" = "armv7l" ]
+    then ARCH="armhf"
+elif [ "$ARCH" = "x86_64" ]
+    then ARCH="amd64"
+elif [ "$ARCH" = "i686" ]
+    then ARCH="i386"
+fi
+sed s/ARCH/$ARCH/ < manifest.json > click/manifest.json
 
 click build click
