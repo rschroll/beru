4434c5946e1bed40ffec7e050a3cc6dd64ed2095
randy-jr-olive <randy.jr.olive@gmail.com>
2014-08-10 19:17:20 -0400

    removed ; (semi-colon) from end of lines, a pedantic warning from cmake

--- qhttpserver/qhttprequest.h
+++ qhttpserver/qhttprequest.h
@@ -56,14 +56,14 @@ class QHttpRequest : public QObject
 {
     Q_OBJECT
 
-    Q_PROPERTY(HeaderHash headers    READ headers);
-    Q_PROPERTY(QString remoteAddress READ remoteAddress);
-    Q_PROPERTY(quint16 remotePort    READ remotePort);
-    Q_PROPERTY(QString method        READ method);
-    Q_PROPERTY(QUrl    url           READ url);
-    Q_PROPERTY(QString path          READ path);
-    Q_PROPERTY(QString httpVersion   READ httpVersion);
-    Q_ENUMS(HttpMethod);
+    Q_PROPERTY(HeaderHash headers    READ headers)
+    Q_PROPERTY(QString remoteAddress READ remoteAddress)
+    Q_PROPERTY(quint16 remotePort    READ remotePort)
+    Q_PROPERTY(QString method        READ method)
+    Q_PROPERTY(QUrl    url           READ url)
+    Q_PROPERTY(QString path          READ path)
+    Q_PROPERTY(QString httpVersion   READ httpVersion)
+    Q_ENUMS(HttpMethod)
 
 public:
     virtual ~QHttpRequest();
