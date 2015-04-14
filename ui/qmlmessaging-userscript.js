d6f35d2c954b2b801494c94505f8d4b705172ad6
Robert Schroll <rschroll@gmail.com>
2015-04-14 15:45:58 -0400

    Remove unused files

--- ui/qmlmessaging-userscript.js
+++
@@ -1,5 +0,0 @@
-oxide.addMessageHandler("MESSAGE", function (msg) {
-    var event = new CustomEvent(msg.args.command, {detail: msg.args.arguments});
-    document.dispatchEvent(event);
-    msg.reply({});
-});
